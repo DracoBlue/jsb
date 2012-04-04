997365cdd811c81b4b7912d3c4ce5d5cdf717d35
DracoBlue <JanS@DracoBlue.de>
2012-04-04 11:51:12 +0300

    Update always on the parent.

--- examples/ajax-include/AjaxInclude.js
+++ examples/ajax-include/AjaxInclude.js
@@ -19,21 +19,16 @@
         $.ajax(request_options).done(function(data)
         {
             var method_name = that.options.method;
+            var parent = that.dom_element.parent();
             if (method_name === 'replace') {
-                var parent = that.dom_element.parent();
                 that.dom_element.replaceWith(data);
-                /*
-                 * Execute all behaviours on it's parent, because
-                 * we just replaced the dom_element.
-                 */
-                jsb.applyBehaviour(parent);
             } else {
                 that.dom_element[method_name](data);
-                /*
-                 * Execute all behaviours on this part now!
-                 */
-                jsb.applyBehaviour(parent);
             }
+            /*
+             * Execute all behaviours on it's parent.
+             */
+            jsb.applyBehaviour(parent);
         });
     };
 
