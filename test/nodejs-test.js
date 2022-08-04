7ce87d113cbf7c911b2dd5b8eb34234953271212
Lars Laade <lars.laade@gmail.com>
2022-08-04 14:13:57 +0200

    update nodejs-test to es6

--- test/nodejs-test.js
+++ test/nodejs-test.js
@@ -1,15 +1,19 @@
 import {
+    describe,
+    it
+} from 'mocha';
+import {
     on,
     fireEvent,
     whenFired,
     fireStickyEvent
 } from '../dist/jsb.es.js';
 
-describe('events', function() {
+describe('events', () => {
 
     it('should fire and catch simple events', function(done) {
-        var counter = 0;
-        on('Event::SIMPLE', function() {
+        let counter = 0;
+        on('Event::SIMPLE', () => {
             counter++;
             if (counter == 2) {
                 done();
@@ -20,9 +24,9 @@ describe('events', function() {
     });
 
     it('should catch events of the past', function(done) {
-        var counter = 0;
+        let counter = 0;
         fireEvent('Event::FROM_THE_PAST');
-        whenFired('Event::FROM_THE_PAST', function() {
+        whenFired('Event::FROM_THE_PAST', () => {
             counter++;
             if (counter == 2) {
                 done();
@@ -32,13 +36,13 @@ describe('events', function() {
     });
 
     it('should detach if off handler is called', function(done) {
-        var counter = 0;
+        let counter = 0;
         this.timeout(1000);
-        var offHandler = on('Event::WITH_OFFHANDLER', function() {
+        let offHandler = on('Event::WITH_OFFHANDLER', () => {
             counter++;
             if (counter == 1) {
                 offHandler();
-                setTimeout(function() {
+                setTimeout(() => {
                     if (counter == 1) {
                         done();
                     } else {
@@ -52,25 +56,25 @@ describe('events', function() {
     });
 
     it('should catch sticky events only once', function(done) {
-        var counter = 0;
+        let counter = 0;
         this.timeout(1000);
         fireStickyEvent('Event::STICKY_EVENT');
-        whenFired('Event::STICKY_EVENT', function() {
+        whenFired('Event::STICKY_EVENT', () => {
             counter++;
         });
 
-        setTimeout(function() {
+        setTimeout(() => {
             if (counter == 1) {
                 done();
             } else {
-                throw new Error('The handler should be called only once, but calls ' + counter + ' times!');
+                throw new Error(`The handler should be called only once, but calls ${counter} times!`);
             }
         }, 200);
     });
 
     it('should catch sticky events only once', function(done) {
-        var counter_one = 0;
-        var counter_two = 0;
+        let counter_one = 0;
+        let counter_two = 0;
         this.timeout(1000);
 
         fireStickyEvent('Event::STICKY_EVENT_WITH_VALUES', {
@@ -83,21 +87,21 @@ describe('events', function() {
 
         whenFired('Event::STICKY_EVENT_WITH_VALUES', {
             'id': 'myid'
-        }, function() {
+        }, () => {
             counter_one++;
         });
 
         whenFired('Event::STICKY_EVENT_WITH_VALUES', {
             'id': 'otherid'
-        }, function() {
+        }, () => {
             counter_two++;
         });
 
-        setTimeout(function() {
+        setTimeout(() => {
             if (counter_one == 1 && counter_two == 1) {
                 done();
             } else {
-                throw new Error('The handler should be called only once, but calls ' + counter_one + ' times for myid and ' + counter_two +' times for otherid!');
+                throw new Error(`The handler should be called only once, but calls ${counter_one} times for myid and ${counter_two} times for otherid!`);
             }
         }, 200);
     });
